6a9237eb1826de080b16e852427b6ddc14461ae7
Vijay Deepak <vijaydeepak7195@outlook.com>
2026-03-16 22:05:38 +0530

    call for events image updated

--- app/data/callForEvents.ts
+++ app/data/callForEvents.ts
@@ -14,13 +14,13 @@ export const callForEvents: CallForEventCardData[] = [
       'D3 Community is hosting meetups!! Would you like to be a speaker at our meetups? Let us know with this form.',
     formUrl:
       'https://docs.google.com/forms/d/e/1FAIpQLSfRlbtegrypHBHw8sHVy_FEcIYLGBo6i5ZETeLxY6NFfSnYjw/viewform',
-    image: '/callForEvents/callForSpeakers.jpeg',
+    image: '/callForEvents/callForSpeakers.png',
   },
   {
     title: 'Call for Sponsors',
     description:
       'D3 Community is hosting meetups!! Would you like to be a sponsor at our meetups? Let us know with this form.',
     formUrl: `mailto:${COMMUNITY_EMAIL}`,
-    image: '/callForEvents/callForSponsors.jpeg',
+    image: '/callForEvents/callForSponsors.png',
   },
 ];
